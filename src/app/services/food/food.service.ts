69f25998b6bc6e3220486b0fc3230e751098901d
Monisasmal <98816752+Monisasmal@users.noreply.github.com>
2022-04-08 00:23:35 +0530

    Update food.service.ts

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -108,7 +108,7 @@ export class FoodService {
         favorite: false,
         origins: ['india'],
         star: 4.6,
-        imageUrl: C:\Users\HP\Desktop\food\Foodorder\dist\foodorder\assets\img1.jpg
+        imageUrl: '/Foodorder/dist/foodorder/assets/img9.jpg'
         tags: ['Green tea']
       },
       {
